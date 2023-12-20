4aca7b1a196f2c5170f4d045b5daae955af17ecd
Even <58809719+tradplus@users.noreply.github.com>
2023-12-20 16:57:51 +0800

    v10.5.0

--- TPExchange/TPExchange.framework/Headers/TradPlusADXInterstitial.h
+++ TPExchange/TPExchange.framework/Headers/TradPlusADXInterstitial.h
@@ -19,6 +19,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (void)showAdFromRootViewController:(UIViewController *)rootViewController;
 @property (nonatomic,assign) BOOL isMute;
+@property (nonatomic,assign) NSInteger skipTime;
 @property (nonatomic,weak) id <TPADXInterstitialDelegate> delegate;
 - (BOOL)isReady;
 
